794d925c919e3063859deac434c48f0d119756af
kfarrel9 <kfarrel9@nd.edu>
2026-09-15 09:56:49 -0400

    Edited text

--- Week_3/my_app.py
+++ Week_3/my_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("Hello, streamlit!")
+st.title("Hello, Streamlit!")
 st.write("This is my first Streamlit app.")
 
 if st.button("Click me"):
@@ -10,4 +10,11 @@ import pandas as pd
 
 st.subheader("Exploring Our Dataset")
 
-df = pd.read_csv("/Users/kfarrell/Documents/Farrell-Data-Science-Portfolio-Fall2026/Week_3/data/sample_data.csv")
+df = pd.read_csv("data/sample_data.csv")
+
+# if in Week 2. this leaves week 3 that we entered in terminal, and now looks from a wider view
+# pd.read_csv("./week_2/data/sample_data.csv")
+
+st.write("Here's our data")
+st.dataframe(df)
+
