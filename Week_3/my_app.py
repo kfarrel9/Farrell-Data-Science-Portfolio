7a46cc24047bf90bd2f48f41152690f5a0adbce6
kfarrel9 <kfarrel9@nd.edu>
2026-09-15 09:44:13 -0400

    Commit updates

--- Week_3/my_app.py
+++ Week_3/my_app.py
@@ -5,3 +5,9 @@ st.write("This is my first Streamlit app.")
 
 if st.button("Click me"):
     st.write("Button clicked!")
+
+import pandas as pd
+
+st.subheader("Exploring Our Dataset")
+
+df = pd.read_csv("/Users/kfarrell/Documents/Farrell-Data-Science-Portfolio-Fall2026/Week_3/data/sample_data.csv")
